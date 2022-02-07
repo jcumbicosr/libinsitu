ebac7683ac6a2a125a3bcf49ec5b7a5ecade454b
Rapĥael Jolivet <raphael.jolivet@mines-paristech.fr>
2022-02-07 15:01:27 +0100

    Update bash scripts

--- main.sh
+++ main.sh
@@ -5,5 +5,12 @@ OUT_FOLDER=out/$NETWORK
 LOGDIR=log
 STATUS_FOLDER=status
 
+if [ -z "$1" ]
+then
+	LIST=`ls -1D $INPUT_DIR/???`
+else
+	LIST="$INPUT_DIR/$1"
+fi
+
 CMD="$PYTHON insitu-etl.py -i -f $STATUS_FOLDER -n $NETWORK -s {2} $OUT_FOLDER/$NETWORK-{2}.nc {1} | tee $LOGDIR/$NETWORK-{2}.log"
-ls -1d $INPUT_DIR/??? | awk '{st=$1; sub(".*/", "", st); print $1 ";" toupper(st)}' | parallel -C ';' $CMD
+echo "$LIST" | awk '{st=$1; sub(".*/", "", st); print $1 ";" toupper(st)}' | parallel -C ';' $CMD
