af59005d411a466d3bff20efc03e11beb322d1cc
Rapĥael Jolivet <raphael.jolivet@mines-paristech.fr>
2022-03-31 15:21:18 +0200

    Update scripts

--- rsync.bash
+++ rsync.bash
@@ -1,5 +1,9 @@
-
-rsync -rt --delete --chmod=644 --chown=tds-insitu:tomcat out/enerMENA/ tds-insitu@brume:/home/thredds_content/thredds/public/enermenastations/
-rsync -rt --delete --chmod=644 --chown=tds-insitu:tomcat out/ABOM/ tds-insitu@brume:/home/thredds_content/thredds/public/bomstations/
-rsync -rt --delete --chmod=644 --chown=tds-insitu:tomcat out/BSRN/ tds-insitu@brume:/home/thredds_content/thredds/public/bsrnstations/
-
+#!/usr/bin/bash
+NETWORKS=("BSRN" "SAURAN"  "enerMENA"  "ABOM")
+FOLDERS=("bsrn" "sauran" "enermena" "bom")
+for i in ${!NETWORKS[@]}
+do
+	network=${NETWORKS[i]}
+	folder=${FOLDERS[i]}
+	rsync -rtL --delete --chmod=644 --chown=tds-insitu:tomcat "out/${network}/" "tds-insitu@brume:/home/thredds_content/thredds/public/${folder}stations/"
+done
