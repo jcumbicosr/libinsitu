605fa813bdd591b10846f35c2c5d50ddf205b69b
Raphael Jolivet <raphael.jolivet@mines-paristech.fr>
2024-06-19 11:01:31 +0200

    Fixing dependencies

--- setup.py
+++ setup.py
@@ -50,7 +50,6 @@ with open("requirements.txt", "r") as f :
                 yield line
 
     requirements = [
-            #str(requirement).replace("==", ">=")
             str(requirement)
             for requirement
             in pkg_resources.parse_requirements(extract_extra_index(f))]
